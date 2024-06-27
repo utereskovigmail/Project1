d115be426d2574f4020ce96de60fae71d2235e51
utereskovigmail <93907050+utereskovigmail@users.noreply.github.com>
2024-06-27 16:07:02 +0200

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    int x=10;
+    int y=5;
+
+    for(int i=y; i<10; i++){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
